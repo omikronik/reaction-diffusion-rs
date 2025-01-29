9cc3810f5262ecd05cd9789d09eadc4f81a51d95
omikronik <yasirceltik9@gmail.com>
2025-01-29 00:29:03 +0000

    added setup functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fmt::Display,
     io::{stdout, Write},
     sync::{
         atomic::{AtomicBool, Ordering},
@@ -8,30 +9,109 @@ use std::{
     time::Duration,
 };
 
+/*
+* █
+* ▓
+* ▒
+* ░
+*
+*/
+
 static DIFFUSION_A: f32 = 1.0;
 static DIFFUSION_B: f32 = 0.5;
 static FEED: f32 = 0.055;
 static KILL: f32 = 0.062;
 static GRID_LENGTH: usize = 50;
 static GRID_HEIGHT: usize = 50;
+static FILL_AMOUNT: f32 = 0.2;
 
-// I want to use
-fn init_grid(grid: &mut [[char; GRID_LENGTH]; GRID_HEIGHT]) {
+static FULL: char = '█';
+static DARK: char = '▓';
+static MEDIUM: char = '▒';
+static LIGHT: char = '░';
+static EMPTY: char = ' ';
+
+#[derive(Debug, Copy, Clone)]
+struct Cell {
+    a: f32,
+    b: f32,
+}
+
+struct Grid {}
+
+fn init_start_grid(grid: &mut [[Cell; GRID_LENGTH]; GRID_HEIGHT]) {
+    // length = 50
+    // find center, x/2 y/2 = 25,25
+    // cover 30% means radius of length 15% of 25 =
+    //let center_x = grid.len().div_floor(2);
+    let center_x: usize = grid.len() / 2;
+    let center_y: usize = grid[0].len() / 2;
+    let radius = ((center_x as f32) * 0.2) as usize;
+    for row in (center_x - radius)..(center_x + radius) {
+        for item in (center_y - radius)..(center_y + radius) {
+            grid[row][item].b = 1.0;
+        }
+    }
+}
+
+fn print_grid(grid: [[Cell; GRID_LENGTH]; GRID_HEIGHT]) {
+    for row in 0..grid.len() {
+        println!("\nROW: {row}");
+        for item in 0..grid[row].len() {
+            print!("{:?},{:?}|", grid[row][item].a, grid[row][item].b)
+        }
+    }
+}
+
+fn print_fancy_grid(grid: [[char; GRID_LENGTH]; GRID_HEIGHT]) {
     for row in 0..grid.len() {
         for item in 0..grid[row].len() {
             print!("{}", grid[row][item]);
         }
-        print!("\n")
+        println!();
+    }
+}
+
+fn fancy_output(grid: [[Cell; GRID_LENGTH]; GRID_HEIGHT]) -> [[char; GRID_LENGTH]; GRID_HEIGHT] {
+    let mut pixels = [[' '; GRID_LENGTH]; GRID_HEIGHT];
+
+    for row in 0..grid.len() {
+        for item in 0..grid[row].len() {
+            let a = grid[row][item].a;
+            let b = grid[row][item].b;
+            let c = ((a - b) * 255.0).floor() as i32;
+            let c = c.clamp(0, 255);
+
+            pixels[row][item] = match c {
+                0..=50 => FULL,
+                51..=101 => DARK,
+                102..=152 => MEDIUM,
+                153..=203 => LIGHT,
+                _ => EMPTY,
+            };
+        }
+    }
+
+    pixels
+}
+
+impl From<(f32, f32)> for Cell {
+    fn from(t: (f32, f32)) -> Cell {
+        Cell { a: t.0, b: t.1 }
     }
 }
 
 fn main() -> std::io::Result<()> {
     let running = Arc::new(AtomicBool::new(false));
 
-    let mut grid: [[char; GRID_LENGTH]; GRID_HEIGHT] = [['1'; GRID_LENGTH]; GRID_HEIGHT];
-    init_grid(&mut grid);
-    let mut next: [[char; GRID_LENGTH]; GRID_HEIGHT] = [['2'; GRID_LENGTH]; GRID_HEIGHT];
-    init_grid(&mut next);
+    let mut grid: [[Cell; GRID_LENGTH]; GRID_HEIGHT] =
+        [[(1.0, 0.0).into(); GRID_LENGTH]; GRID_HEIGHT];
+    init_start_grid(&mut grid);
+    let mut next: [[Cell; GRID_LENGTH]; GRID_HEIGHT] =
+        [[(1.0, 0.0).into(); GRID_LENGTH]; GRID_HEIGHT];
+
+    let fancy_grid = fancy_output(grid);
+    print_fancy_grid(fancy_grid);
 
     signal_hook::flag::register(signal_hook::consts::SIGINT, Arc::clone(&running))?;
 
@@ -43,7 +123,7 @@ fn main() -> std::io::Result<()> {
         let _ = handle.write_all(format!("{i}\n").as_bytes());
         handle.flush()?;
         i += 1;
-        thread::sleep(Duration::from_millis(100));
+        thread::sleep(Duration::from_millis(1000));
     }
 
     Ok(())
